8e4c66fba1c0e05cde67d949868e395e408a9c4a
Heinz <heinz.schaarschmidt@freenet.de>
2020-05-13 18:00:57 +0200

    implemented new data format filter and applied it to transmitted data. Now we have the whole EPROM dump here!

--- juniorcopy/Program.cs
+++ juniorcopy/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.IO;
 using System.IO.Ports;
 using System.Threading;
@@ -7,39 +8,47 @@ using System.Threading.Tasks;
 namespace juniorcopy
 {
     // Console program which 
-    // - reads from COM3: or COM4: and writes to ./juniorReceived.bin (not yet)
+    // - reads from COM3: or COM4: and writes to ./"juniorRcv.txt" in "7+7 format"
     // OR: 
     // - reads a given file and writes its contents to COM3: or COM4:
-    public class PortChat
+    // OR:
+    // - converts a text file in "7+7 format" to a binary file which then contains the original junior memory data.
+    public class Program
     {
         static bool _continue;
 
         public static void Main(string[] args)
         {
             Console.Error.WriteLine("Hi, hier ist juniorcopy");
-            if (!(args.Length == 0 || args.Length == 2 && args[0] == "-s"))
+            if (!(args.Length == 0 || args.Length == 2 && args[0] == "-s" || args.Length == 2 && args[0] == "-772bin"))
             {
-                Console.Error.WriteLine("Usage: juniorCopy [-s <file to send>]");
+                Console.Error.WriteLine("Usage: juniorCopy [-s <file to send>] [-772bin <input text file> <output binary file>]");
                 return;
             }
 
-            string[] ports = SerialPort.GetPortNames();
-            if (ports.Length == 0)
+            SerialPort serialPort = null;
+            if (args.Length == 0 || args[0] == "-s")
             {
-                Console.Error.WriteLine("Keine seriellen Ports gefunden. Bye!");
-                return;
-            }
-            // else
-            Console.Error.WriteLine("Ports: " + string.Join(",", ports));
+                string[] ports = SerialPort.GetPortNames();
+                if (ports.Length == 0)
+                {
+                    Console.Error.WriteLine("Keine seriellen Ports gefunden. Bye!");
+                    return;
+                }
+                // else
+                Console.Error.WriteLine("Ports: " + string.Join(",", ports));
 
-            SerialPort serialPort = OpenSerialPort(ports);
+                serialPort = OpenSerialPort(ports);
+            }
 
             if (args.Length == 0)
                 ReceiveFromJunior(serialPort);
-            else
+            else if (args[0] == "-s")
                 SendToJunior(serialPort, args[1]);
+            else
+                Convert772bin(serialPort, args[1]);
 
-            serialPort.Close();
+            serialPort?.Close();
             Console.Error.WriteLine("bye!");
         }
 
@@ -94,7 +103,7 @@ namespace juniorcopy
             Thread.Sleep(1000);
         }
 
-        public static void ReceiveFromJunior(SerialPort port)
+        private static void ReceiveFromJunior(SerialPort port)
         {
             Console.Error.WriteLine("receiving from junior on {0}...", port.PortName);
 
@@ -122,7 +131,7 @@ namespace juniorcopy
             sw.Close();
         }
 
-        public static void Read(SerialPort port, StreamWriter sw)
+        private static void Read(SerialPort port, StreamWriter sw)
         {
             while (_continue)
             {
@@ -138,6 +147,48 @@ namespace juniorcopy
                     Console.Write(".");
                 }
             }
+        }
+
+        // read input text, convert each group like "54 29 " to an 8-bit-byte, and write it to output file
+        private static void Convert772bin(SerialPort serialPort, string inputFileName)
+        {
+            string outputFileName = inputFileName + ".bin";
+            using (StreamReader sr = new StreamReader(inputFileName))
+            using (FileStream outFile = File.OpenWrite(outputFileName))
+            {
+                char[] buffer = new char[6]; // input format: like "54 29 "
+                byte[] result = new byte[1]; // output format: one byte ("54 29 "-> 0xA9)
+                int pos = 0;
+                do
+                {
+                    int readCount = sr.Read(buffer, 0, 6);
+                    if (readCount != 6)
+                    {
+                        Console.Error.WriteLine("conversion stopped at pos 0x{0:X}: readCount={1}", pos, readCount);
+                        break;
+                    }
+                    pos++;
+
+                    string hexValues = new string(buffer);
+                    string[] hexValuesSplit = hexValues.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+                    if (hexValuesSplit.Length != 2 || hexValuesSplit[0].Length != 2 || hexValuesSplit[1].Length != 2)
+                        Console.Error.WriteLine("pos 0x{0:X}: strange 6 bytes: {1}", pos, hexValues);
+
+                    int[] values = hexValuesSplit.Select(hex => Convert.ToInt32(hex, 16)).ToArray();
+                    int first = values[0];
+                    int second = values[1];
+
+                    // middle 6 bits must match!
+                    if ((second >> 1) != (first & 0x3F))
+                        Console.Error.WriteLine("pos 0x{0:X}: strange 6 bytes: {1}", pos, hexValues);
+
+                    int combined = (first << 1) | second;
+                    //Console.WriteLine("hex {0} -> int {1:X2} {2:X2} -> {3:X2}", hexValues, first, second, combined);
+                    result[0] = (byte)combined;
+                    outFile.Write(result, 0, 1);
+
+                } while (true);
+            } // using
         }
     }
 }
