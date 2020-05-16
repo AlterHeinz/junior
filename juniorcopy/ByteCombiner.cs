c645acd0bd40c1b8780e89a53ac5c08a99dbc73d
Heinz <heinz.schaarschmidt@freenet.de>
2020-05-16 18:43:52 +0200

    receiving made easier; eliminated intermediate file

---
+++ juniorcopy/ByteCombiner.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+namespace juniorcopy
+{
+    // checks 2 consecutive 7-bit-bytes for consistency, 
+    // combines them to an 8-bit-byte (like 0x54 and 0x29 -> 0xA9) and writes that to the output stream
+    public class ByteCombiner : IObserver<byte>
+    {
+        public ByteCombiner(Stream output)
+        {
+            this.output = output;
+        }
+
+        public void OnCompleted()
+        {
+            if (!atFirst)
+            {
+                Console.Error.WriteLine("pos 0x{0:X}: obsolete final value: {1:X2}", pos, first);
+                Forward(first);
+            }
+        }
+
+        public void OnError(Exception ex)
+        {
+            throw new NotImplementedException("unexpected:", ex);
+        }
+
+        public void OnNext(byte value)
+        {
+            if (atFirst)
+            {
+                first = value;
+                atFirst = false;
+            }
+            else
+            {
+                Forward(CheckAndCombine(first, value, pos));
+                pos++;
+                atFirst = true;
+            }
+        }
+
+        // convert one group like "54 29 " to an 8-bit-byte ("54 29 "-> 0xA9)
+        public static byte CheckAndCombine(byte first, byte second, int pos)
+        {
+            // middle 6 bits must match!
+            if ((first & 0x3F) != (second >> 1))
+                Console.Error.WriteLine("pos 0x{0:X}: strange values: {1:X2} {2:X2}", pos, first, second);
+
+            return (byte)((first << 1) | second);
+        }
+
+        private void Forward(byte value)
+        {
+            output.WriteByte(value);
+        }
+
+        private Stream output;
+        private byte first;
+        private bool atFirst = true;
+        private int pos = 0;
+    }
+}
--- juniorcopy/Program.cs
+++ juniorcopy/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.IO;
 using System.IO.Ports;
 using System.Threading;
@@ -18,33 +17,27 @@ namespace juniorcopy
         public static void Main(string[] args)
         {
             Console.Error.WriteLine("Hi, i'm juniorcopy");
-            if (!(args.Length == 0 || args.Length == 2 && args[0] == "-s" || args.Length == 2 && args[0] == "-772bin"))
+            if (!(args.Length == 0 || args.Length == 2 && args[0] == "-s"))
             {
-                Console.Error.WriteLine("Usage: juniorCopy [-s <file to send>] [-772bin <input text file> <output binary file>]");
+                Console.Error.WriteLine("Usage: juniorCopy [-s <file to send>]");
                 return;
             }
 
-            SerialPort serialPort = null;
-            if (args.Length == 0 || args[0] == "-s")
+            string[] ports = SerialPort.GetPortNames();
+            if (ports.Length == 0)
             {
-                string[] ports = SerialPort.GetPortNames();
-                if (ports.Length == 0)
-                {
-                    Console.Error.WriteLine("No serial ports found. Bye!");
-                    return;
-                }
-                // else
-                Console.Error.WriteLine("Ports: " + string.Join(",", ports));
-
-                serialPort = OpenSerialPort(ports);
+                Console.Error.WriteLine("No serial ports found. Bye!");
+                return;
             }
+            // else
+            Console.Error.WriteLine("Ports: " + string.Join(",", ports));
+
+            SerialPort serialPort = OpenSerialPort(ports);
 
             if (args.Length == 0)
                 ReceiveFromJunior(serialPort);
-            else if (args[0] == "-s")
-                SendToJunior(serialPort, args[1]);
             else
-                Convert772bin(args[1]);
+                SendToJunior(serialPort, args[1]);
 
             serialPort?.Close();
             Console.Error.WriteLine("bye!");
@@ -58,7 +51,7 @@ namespace juniorcopy
             serialPort.BaudRate = 4800;
             serialPort.Parity = Parity.None;
             serialPort.DataBits = 8;
-            serialPort.StopBits = StopBits.One;
+            serialPort.StopBits = StopBits.Two;
             serialPort.Handshake = Handshake.None;
             serialPort.ReadTimeout = 1000;
 
@@ -94,86 +87,45 @@ namespace juniorcopy
         {
             Console.Error.WriteLine("receiving from junior on {0}...", port.PortName);
 
-            const string textFileName = "juniorRcv.txt";
-            using (StreamWriter sw = new StreamWriter(textFileName, false))
+            const string outputFileName = "juniorRcv.bin";
+            using (FileStream outFile = File.OpenWrite(outputFileName))
             {
-                using (CancellationTokenSource cts = new CancellationTokenSource())
+                var combiner = new ByteCombiner(outFile);
+                try
                 {
-                    Task t = Task.Factory.StartNew(() => Read(port, sw, cts.Token));
+                    using (CancellationTokenSource cts = new CancellationTokenSource())
+                    {
+                        Task t = Task.Factory.StartNew(() => Read(port, combiner, cts.Token));
 
-                    Console.Error.WriteLine("junior may start sending now!");
-                    Console.Error.WriteLine("press ENTER to finish conversion and exit - after transmission has completed.");
-                    string name = Console.ReadLine();
-                    cts.Cancel();
-                    t.Wait();
+                        Console.Error.WriteLine("junior may start sending now!");
+                        Console.Error.WriteLine("press ENTER to finish conversion and exit - after transmission has completed.");
+                        Console.ReadLine();
+                        cts.Cancel();
+                        t.Wait();
+                    }
+                }
+                finally
+                {
+                    combiner.OnCompleted();
                 }
             }
-            Console.Error.WriteLine("created intermediate file {0}", textFileName);
-
-            // create binary file, too
-            Convert772bin(textFileName);
+            Console.Error.WriteLine("created binary file {0}", outputFileName);
         }
 
-        private static void Read(SerialPort port, StreamWriter sw, CancellationToken ct)
+        private static void Read(SerialPort port, IObserver<byte> consumer, CancellationToken ct)
         {
             while (!ct.IsCancellationRequested)
             {
                 try
                 {
-                    int read = port.ReadChar();
+                    byte read = (byte)port.ReadChar();
                     Console.Write("{0:X2} ", read);
-                    sw.Write("{0:X2} ", read);
+                    consumer.OnNext(read);
                 }
                 catch (TimeoutException) {
                     Console.Write(".");
                 }
             }
-        }
-
-        // read input text, convert each group like "54 29 " to an 8-bit-byte, and write it to output file
-        private static void Convert772bin(string inputFileName)
-        {
-            string outputFileName = inputFileName + ".bin";
-            using (StreamReader sr = new StreamReader(inputFileName))
-            using (FileStream outFile = File.OpenWrite(outputFileName))
-            {
-                char[] buffer = new char[6]; // input format: like "54 29 "
-                byte[] result = new byte[1]; // output format: one byte ("54 29 "-> 0xA9)
-                for (int pos = 0; true; pos++)
-                {
-                    int readCount = sr.Read(buffer, 0, 6);
-                    if (readCount != 6)
-                    {
-                        Console.Error.WriteLine("conversion stopped at pos 0x{0:X}: readCount={1}", pos, readCount);
-                        break;
-                    }
-
-                    result[0] = Convert772bin(buffer, pos);
-                    outFile.Write(result, 0, 1);
-                }
-            } // using
-            Console.Error.WriteLine("created binary file {0}", outputFileName);
-        }
-
-        // convert one group like "54 29 " to an 8-bit-byte ("54 29 "-> 0xA9)
-        private static byte Convert772bin(char[] buffer, int pos)
-        {
-            string hexValues = new string(buffer);
-            string[] hexValuesSplit = hexValues.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-            if (hexValuesSplit.Length != 2 || hexValuesSplit[0].Length != 2 || hexValuesSplit[1].Length != 2)
-                Console.Error.WriteLine("pos 0x{0:X}: strange 6 bytes: {1}", pos, hexValues);
-
-            int[] values = hexValuesSplit.Select(hex => Convert.ToInt32(hex, 16)).ToArray();
-            int first = values[0];
-            int second = values[1];
-
-            // middle 6 bits must match!
-            if ((second >> 1) != (first & 0x3F))
-                Console.Error.WriteLine("pos 0x{0:X}: strange 6 bytes: {1}", pos, hexValues);
-
-            int combined = (first << 1) | second;
-            //Console.WriteLine("hex {0} -> int {1:X2} {2:X2} -> {3:X2}", hexValues, first, second, combined);
-            return (byte)combined;
         }
     }
 }
